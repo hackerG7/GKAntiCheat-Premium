9f36cfbb3eeaccd0b47f9160088525b7af741092
hackerG7 <45177413+hackerG7@users.noreply.github.com>
2020-03-05 09:34:00 +0800

    first upload

---
+++ src/me/club/gkanticheat/PlayerData.java
@@ -0,0 +1,24 @@
+package me.club.gkanticheat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.UUID;
+
+
+public class PlayerData{
+	public UUID uid = null;
+	public String playerName = "";
+	public String key = "";
+	public static String generateCode() {
+		return "bypass: "+Math.random()*1000000+UUID.randomUUID().toString().replace("-", "").substring(0, 12);
+	}
+	public PlayerData(UUID uid, String playerName) {
+		this.uid = uid;
+		this.playerName = playerName;
+		key = generateCode();
+	}
+
+}
